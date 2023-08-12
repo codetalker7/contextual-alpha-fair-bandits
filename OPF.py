a49e1a2aafb7d4ac657a4b53fbd4959779b2d16c
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-12 19:15:34 +0530

    Adding docstring in OPF.py and minor fix.

--- OPF.py
+++ OPF.py
@@ -4,6 +4,11 @@ import numpy as np
 import math
 
 class OPF(Policy):
+    """
+    :param num_contexts: Number of contexts.
+    :param num_arms: Number of arms.
+    :param alpha: Degree of fairness to be used in the concave fair function.
+    """
     def __init__(self, num_contexts, num_arms, alpha):
         super().__init__(num_contexts, num_arms)
         self.next_prediction = np.ones((self.num_arms, ))   # ignore contexts
@@ -12,8 +17,12 @@ class OPF(Policy):
 
     def decision(self, context=None): 
         # ignore context
-        return self.next_prediction
+        return np.random.choice(self.num_arms, p=self.next_prediction) + 1
 
+    """
+    :param numpy.ndarray rewards: Vector representing rewards for each arm.
+    :param numpy.ndarray cumulative_rewards: Vector representing the cumulative rewards accrued till the current time step.
+    """
     def feedback(self, rewards, cumulative_rewards):
         # compute gradient
         gradient = rewards / np.power(cumulative_rewards, self.alpha)
