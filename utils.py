3eb55d5bd472c696a032791df4961566979fb064
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-12 19:15:56 +0530

    Adding implementation of projection on simplex.

--- utils.py
+++ utils.py
@@ -1,2 +1,18 @@
-def projectOnSimplex(vector):
-    pass
+import numpy as np
+
+def projectOnSimplex(v):
+    n, = v.shape
+    # check if we are already on the simplex
+    if v.sum() == 1 and np.alltrue(v >= 0):
+        # best projection: itself!
+        return v
+    # get the array of cumulative sums of a sorted (decreasing) copy of v
+    u = np.sort(v)[::-1]
+    cssv = np.cumsum(u)
+    # get the number of > 0 components of the optimal solution
+    rho = np.nonzero(u * np.arange(1, n+1) > (cssv - 1))[0][-1]
+    # compute the Lagrange multiplier associated to the simplex constraint
+    theta = float(cssv[rho] - 1) / (rho + 1)
+    # compute the projection by thresholding v using theta
+    w = (v - theta).clip(min=0)
+    return w
