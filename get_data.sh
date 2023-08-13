022745ea6c0d0bf26d6f00be742637f01479b6b2
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-13 14:50:01 +0530

    Adding script for downloading data.

---
+++ get_data.sh
@@ -0,0 +1,5 @@
+rm -rf data
+mkdir data
+
+wget https://files.grouplens.org/datasets/movielens/ml-25m-README.html -O data/ml-25m-README.html
+wget https://files.grouplens.org/datasets/movielens/ml-25m.zip -O data/ml-25m.zip
