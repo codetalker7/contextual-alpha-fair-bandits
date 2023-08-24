b3b849291cfec2a2694b219c7a381d5671654e39
Siddhant Chaudhary <urssidd@gmail.com>
2023-08-24 18:38:22 +0530

    Adding implementation of our bandit policy.

---
+++ ParallelScaleFreeMAB.py
@@ -0,0 +1,33 @@
+import numpy as np
+import utils
+
+from BanditPolicy import BanditPolicy
+from ScaleFreeMAB import ScaleFreeMAB
+
+class ParallelScaleFreeMAB(BanditPolicy):
+    """
+    :param int num_contexts: Number of contexts.
+    :param int num_arms: Number of arms.
+    """
+    def __init__(self, num_contexts, num_arms, alpha):
+        super().__init__(num_contexts, num_arms)
+        self.alpha = alpha
+        self.cumulative_rewards = np.ones((num_arms, ))
+        self.parallel_bandit_policies = [ScaleFreeMAB(num_contexts, num_arms) for i in range(num_contexts)]
+        self.last_chosen_arm = 1
+        self.last_context = 0
+
+    def decision(self, context):
+        self.last_context = context
+        self.last_chosen_arm = self.parallel_bandit_policies[context].decision(context)
+        return self.last_chosen_arm
+
+    def feedback(self, reward):
+        # feedback modified reward to underlying policy
+        modified_reward = (1 / (self.cumulative_rewards[self.last_chosen_arm - 1]) ** self.alpha) * reward
+
+        self.parallel_bandit_policies[self.last_context].feedback(modified_reward)
+
+        # update cumulative reward for picked arm
+        self.cumulative_rewards[self.last_chosen_arm - 1] += reward
+
