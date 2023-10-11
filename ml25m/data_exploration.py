d61bd929c9dbd1dbf63414e09b11b260b3b1c450
Siddhant Chaudhary <urssidd@gmail.com>
2023-10-11 19:08:56 +0530

    Adding an option to use the timestamps in the dataset to sort the rows.

--- ml25m/data_exploration.py
+++ ml25m/data_exploration.py
@@ -1,13 +1,10 @@
-import numpy as np
-from numpy.random import random
 import pandas as pd
 import argparse
-
-from pandas.core.common import random_state
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ROWS', dest='ROWS', default=50000, help='Number of rows of the dataset to use.')
 parser.add_argument('--SEED', dest='SEED', default=42, help='Random seed to shuffle the dataset.')
+parser.add_argument('--USETIMESTAMPS', dest='USETIMESTAMPS', default=False, help='Boolean determining whether the timestamps given in the dataset will be used to shuffle the rows.')
 args = parser.parse_args()
 
 data = pd.read_csv("data/ml-25m/ratings.csv")
@@ -16,8 +13,11 @@ movies = movies.set_index("movieId")
 
 ROWS = int(args.ROWS)
 SEED = int(args.SEED)
+USETIMESTAMPS = bool(args.USETIMESTAMPS)
+
 print("Number of rows to use: ", ROWS)
-print("Random seed to shuffle dataset: ", SEED)
+print("Use timestamps to shuffle rows?: ", USETIMESTAMPS)
+print("Random seed to shuffle dataset (if USETIMESTAMPS is false): ", SEED)
 
 ## work with the first ROWS rows
 data = data.iloc[:ROWS]
@@ -33,8 +33,11 @@ def filter_movies(movieId):
 
 data = data[data.apply(lambda row: filter_movies(row["movieId"]), axis=1)]
 
-# randomly shuffle the rows (to shuffle the contexts)
-data = data.sample(frac=1, random_state=SEED).reset_index(drop=True)
+# randomly shuffle the rows (to shuffle the contexts), or USETIMESTAMPS to shuffle rows
+if USETIMESTAMPS:
+    data = data.sort_values(by=['timestamp']).reset_index(drop=True)
+else:
+    data = data.sample(frac=1, random_state=SEED).reset_index(drop=True)
 
 categories = set()
 
