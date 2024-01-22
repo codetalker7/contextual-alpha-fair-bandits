9ef47d8cdde45ea52c7eb20a90c973b4cb193bf2
Siddhant Chaudhary <urssidd@gmail.com>
2024-01-22 17:19:35 +0530

    Using config in offline_optimal.py.

--- ml25m/offline_optimal.py
+++ ml25m/offline_optimal.py
@@ -1,34 +1,9 @@
 import cvxpy as cp
-import numpy as np
 import pandas as pd
 import pickle
-import argparse
-import random
+import json
+import numpy as np
 from tqdm import tqdm
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--ALPHA', dest='ALPHA', default=0.5, help='Fairness level')
-parser.add_argument('--SMALLREWARD', dest='SMALL_REWARD', default=0.001, help='Very small reward for the bad arm.')
-args = parser.parse_args()
-
-with open("data.pickle", 'rb') as f:
-    data = pickle.load(f)
-
-with open("categories.pickle", 'rb') as f:
-    categories = list(pickle.load(f))
-
-movies = pd.read_csv("data/ml-25m/movies.csv")
-movies = movies.set_index("movieId")
-
-# temporary constants
-NUM_CONTEXTS = len(data["userId"].unique())
-NUM_ARMS = len(categories)
-ALPHA = float(args.ALPHA)
-SMALL_REWARD = float(args.SMALL_REWARD)
-
-# printing args
-print("ALPHA: ", ALPHA)
-print("SMALL_REWARD: ", SMALL_REWARD)
 
 def get_rewards(movieId):
     genres = movies.loc[movieId]["genres"].split("|")
@@ -42,16 +17,44 @@ def get_rewards(movieId):
 
     return rewards
 
-# sequence of contexts and reward vectors
-context_sequence = list(data["userId"])
-reward_sequence = []
+# load the config and stats
+with open('config.json', 'r') as f:
+    config_dict = json.load(f)
 
-for t in range(len(data)):
-    data_point = data.iloc[t]
-    movieId = int(data_point["movieId"])
-    reward_sequence.append(get_rewards(movieId))
+with open('stats.json', 'r') as f:
+    stats_dict = json.load(f)
+
+# load the data and categories
+with open(config_dict["DATAPATH"], 'rb') as f:
+    data = pickle.load(f)
+
+with open(config_dict["CATEGORIES_PATH"],'rb') as f:
+    categories = list(pickle.load(f))
+
+# load the user_to_index and index_to_user maps
+with open(config_dict["USER_TO_INDEX_PATH"], 'rb') as f:
+    map_user_to_index = pickle.load(f)
+
+with open(config_dict["INDEX_TO_USER_PATH"], 'rb') as f:
+    map_index_to_user = pickle.load(f)
+
+# load the movie dataset
+movies = pd.read_csv("data/ml-25m/movies.csv")
+movies = movies.set_index("movieId")
+
+# temporary constants
+NUM_CONTEXTS = int(stats_dict["NUM_CONTEXTS"])
+NUM_ARMS = int(stats_dict["NUM_ARMS"])
+ALPHA = float(config_dict["ALPHA"])
+SMALL_REWARD = float(config_dict["SMALLREWARD"])
+if (config_dict["USETIMESTAMPS"] == 'True'):
+    USETIMESTAMPS = True
+else:
+    USETIMESTAMPS = False
+
+# the reward function to use
+reward_function = get_rewards
     
-
 # need one vector variable for each constraint
 variables = [cp.Variable(NUM_ARMS) for i in range(NUM_CONTEXTS)]
 
@@ -67,11 +70,15 @@ cumulative_rewards = [cp.expressions.constants.Constant(1) for i in range(NUM_AR
 offline_optimal_values = []
 
 for t in tqdm(range(len(data))):
+    data_point = data.iloc[t]
+    movieId = int(data_point["movieId"])
+    context_t = map_user_to_index[int(data_point["userId"])]
+    rewards_t = reward_function(movieId)
+
     objective_function = cp.expressions.constants.Constant(0)
 
-    context_t = context_sequence[t]
     for i in range(NUM_ARMS):
-        cumulative_rewards[i] += reward_sequence[t][i] * variables[context_t - 1][i]
+        cumulative_rewards[i] += rewards_t[i] * variables[context_t][i]
         objective_function += (cumulative_rewards[i] ** (1 - ALPHA)) / (1 - ALPHA)
 
     obj = cp.Minimize(-objective_function)
@@ -81,5 +88,6 @@ for t in tqdm(range(len(data))):
     offline_optimal_values.append(-problem.value)
 
 # saving the offline optimal values
-with open("offline_optimal.pickle", "wb") as f:
+filename = f'pickled_files/offline_optimal_alpha={ALPHA}_smallreward={SMALL_REWARD}_usetimestamps={USETIMESTAMPS}.pickle'
+with open(filename, "wb") as f:
     pickle.dump(offline_optimal_values, f)
