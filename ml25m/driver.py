d75c926089382773aa4545af626e0ee557974490
Siddhant Chaudhary <urssidd@gmail.com>
2024-01-22 17:30:27 +0530

    Using config in driver.

--- ml25m/driver.py
+++ ml25m/driver.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import pickle
-import argparse
 import sys
+import json
 from tqdm import tqdm
 
 ## adding path for the policy classes
@@ -10,33 +10,45 @@ from os.path import dirname, abspath
 class_path = dirname(dirname(abspath(__file__)))
 sys.path.append(class_path)
 
-## command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--ALPHA', dest='ALPHA', default=0.5, help='Fairness level')
-parser.add_argument('--SMALLREWARD', dest='SMALL_REWARD', default=0.001, help='Very small reward for the bad arm.')
-parser.add_argument('--SEED', dest='SEED', default=42, help='Random seed to have reproducible results.')
-args = parser.parse_args()
+## loading the config and stats
+with open("config.json", "r") as f:
+    config_dict = json.load(f)
 
+with open("stats.json", "r") as f:
+    stats_dict = json.load(f)
 
 ## loading the datasets
-with open("data.pickle", 'rb') as f:
+with open(config_dict["DATAPATH"], 'rb') as f:
     data = pickle.load(f)
 
-with open("categories.pickle", 'rb') as f:
+with open(config_dict["CATEGORIES_PATH"], 'rb') as f:
     categories = list(pickle.load(f))
 
+## load the user_to_index and index_to_user maps
+with open(config_dict["USER_TO_INDEX_PATH"], 'rb') as f:
+    map_user_to_index = pickle.load(f)
+
+with open(config_dict["INDEX_TO_USER_PATH"], 'rb') as f:
+    map_index_to_user = pickle.load(f)
+
+## load the movie dataset
 movies = pd.read_csv("data/ml-25m/movies.csv")
 movies = movies.set_index("movieId")
 
 ## setting globals
-NUM_CONTEXTS = len(data["userId"].unique())
-NUM_ARMS = len(categories)
-ALPHA = float(args.ALPHA)
-SMALL_REWARD = float(args.SMALL_REWARD)
+NUM_CONTEXTS = int(stats_dict["NUM_CONTEXTS"])
+NUM_ARMS = int(stats_dict["NUM_ARMS"])
+SEED = int(config_dict["SEED"])
+ALPHA = float(config_dict["ALPHA"])
+SMALL_REWARD = float(config_dict["SMALLREWARD"])
 APPROX_FACTOR = (1 - ALPHA) ** (-(1 - ALPHA)) 
+if (config_dict["USETIMESTAMPS"] == 'True'):
+    USETIMESTAMPS = True
+else:
+    USETIMESTAMPS = False
 
 ## random seed for numpy
-np.random.seed(int(args.SEED))
+np.random.seed(SEED)
 
 ## logging some stats
 print("ARMS: ", NUM_ARMS)
@@ -44,11 +56,12 @@ print("CONTEXTS: ", NUM_CONTEXTS)
 print("ALPHA: ", ALPHA)
 print("APPROXIMATION FACTOR: ", APPROX_FACTOR)
 print("SMALL_REWARD: ", SMALL_REWARD)
-print("SEED: ", int(args.SEED))
+print("SEED: ", SEED)
+print("USETIMESTAMPS:", USETIMESTAMPS)
 
-## getting the offline optimal objectives
-with open("offline_optimal.pickle", "rb") as f:
-    offline_optimal_values = pickle.load(f)
+# ## getting the offline optimal objectives
+# with open("offline_optimal.pickle", "rb") as f:
+#     offline_optimal_values = pickle.load(f)
 
 ## function to compute rewards for a movie
 def get_rewards(movieId):
@@ -62,3 +75,6 @@ def get_rewards(movieId):
             rewards[index] = SMALL_REWARD
 
     return rewards
+
+## setting the reward function
+reward_function = get_rewards
