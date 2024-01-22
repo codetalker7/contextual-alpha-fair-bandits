c0cd0fb0d1152df807fb3699d5c673acd52aa55b
Siddhant Chaudhary <urssidd@gmail.com>
2024-01-22 17:30:42 +0530

    Cleaning offline_optimal.py and using driver.py instead.

--- ml25m/offline_optimal.py
+++ ml25m/offline_optimal.py
@@ -1,59 +1,6 @@
 import cvxpy as cp
-import pandas as pd
-import pickle
-import json
-import numpy as np
 from tqdm import tqdm
-
-def get_rewards(movieId):
-    genres = movies.loc[movieId]["genres"].split("|")
-    rewards = np.zeros((NUM_ARMS, ))
-
-    for index in range(len(categories)):
-        if categories[index] in genres:
-            rewards[index] = 1
-        else:
-            rewards[index] = SMALL_REWARD
-
-    return rewards
-
-# load the config and stats
-with open('config.json', 'r') as f:
-    config_dict = json.load(f)
-
-with open('stats.json', 'r') as f:
-    stats_dict = json.load(f)
-
-# load the data and categories
-with open(config_dict["DATAPATH"], 'rb') as f:
-    data = pickle.load(f)
-
-with open(config_dict["CATEGORIES_PATH"],'rb') as f:
-    categories = list(pickle.load(f))
-
-# load the user_to_index and index_to_user maps
-with open(config_dict["USER_TO_INDEX_PATH"], 'rb') as f:
-    map_user_to_index = pickle.load(f)
-
-with open(config_dict["INDEX_TO_USER_PATH"], 'rb') as f:
-    map_index_to_user = pickle.load(f)
-
-# load the movie dataset
-movies = pd.read_csv("data/ml-25m/movies.csv")
-movies = movies.set_index("movieId")
-
-# temporary constants
-NUM_CONTEXTS = int(stats_dict["NUM_CONTEXTS"])
-NUM_ARMS = int(stats_dict["NUM_ARMS"])
-ALPHA = float(config_dict["ALPHA"])
-SMALL_REWARD = float(config_dict["SMALLREWARD"])
-if (config_dict["USETIMESTAMPS"] == 'True'):
-    USETIMESTAMPS = True
-else:
-    USETIMESTAMPS = False
-
-# the reward function to use
-reward_function = get_rewards
+from driver import *
     
 # need one vector variable for each constraint
 variables = [cp.Variable(NUM_ARMS) for i in range(NUM_CONTEXTS)]
