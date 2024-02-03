c1f2721d6c7268b310ed99ff3d0888e2218bc458
Siddhant Chaudhary <urssidd@gmail.com>
2024-02-03 19:50:55 +0530

    Saving the fairness values for different mus.

--- ml25m/dependence_on_mu.py
+++ ml25m/dependence_on_mu.py
@@ -1,12 +1,6 @@
 from driver import *
 
-NUM_POLICIES = 101
-PLOTREGRET = config_dict["PLOTREGRET"]
-
-## getting the offline optimal objectives
-if PLOTREGRET:
-    with open(OFFLINE_OPTIMAL_FILE, "rb") as f:
-        offline_optimal_values = pickle.load(f)
+NUM_POLICIES = 51
 
 from ParallelOPF import ParallelOPF
 from FairCB import FairCB
@@ -29,21 +23,8 @@ policies.append(ParallelOPF(NUM_CONTEXTS, NUM_ARMS, ALPHA)) # last policy is our
 ## keeping track of cumulative rewards
 cumulative_rewards = [[np.ones(NUM_ARMS, )] for i in range(len(policies))]
 
-## alpha-performance
-alpha_performance = [[] for i in range(len(policies))]
-
 ## jain's fairness index
 fairness_index = [[] for i in range(len(policies))]
-
-## sum of rewards
-sum_rewards = [[0] for i in range(len(policies))]
-
-if PLOTREGRET:
-    ## standard regrets
-    standard_regrets = [[] for i in range(len(policies))]
-
-    ## approximate regrets
-    approximate_regrets = [[] for i in range(len(policies))]
 
 for t in tqdm(range(len(data))):
     data_point = data.iloc[t]
@@ -55,35 +36,47 @@ for t in tqdm(range(len(data))):
     ## get rewards corresponding to the movie
     rewards = reward_function(movieId)
 
-    ## update performance
-    for i in range(len(policies)):
-        sum_rewards[i].append(sum_rewards[i][-1] + rewards[recommended_genres[i] - 1])
-
     ## update cumulative rewards
     for i in range(len(policies)):
         last_cum_rewards = cumulative_rewards[i][-1]
         cumulative_rewards[i].append(last_cum_rewards + rewards * policies[i].last_decision)
-
-    ## updating alpha-performance
-    for i in range(len(policies)):
-        last_cum_rewards = cumulative_rewards[i][-1]
-        alpha_performance[i].append((last_cum_rewards ** (1 - ALPHA) / (1 - ALPHA)).sum())
 
     ## update the fairness index
     for i in range(len(policies)):
         last_cum_rewards = cumulative_rewards[i][-1]
         fairness_index[i].append(jains_fairness_index(last_cum_rewards))
 
-    if PLOTREGRET:
-        ## update the standard regrets
-        for i in range(len(policies)):
-            last_cum_rewards = cumulative_rewards[i][-1]
-            standard_regrets[i].append(offline_optimal_values[t] - (last_cum_rewards ** (1 - ALPHA) / (1 - ALPHA)).sum())
-        ## update the approximate regret
-        for i in range(len(policies)):
-            last_cum_rewards = cumulative_rewards[i][-1]
-            approximate_regrets[i].append(offline_optimal_values[t] - APPROX_FACTOR * (last_cum_rewards ** (1 - ALPHA) / (1 - ALPHA)).sum())
-
     ## feedback rewards to the policies
     for i in range(len(policies)):
         policies[i].feedback(rewards)
+
+FAIRNESS_VALUES_FILE = f'pickled_files/fairness_values_rows={config_dict["ROWS"]}_seed={SEED}_alpha={ALPHA}_smallreward={SMALL_REWARD}_usetimestamps={USETIMESTAMPS}_frequency={config_dict["FREQUENCY"]}_highfrequency={config_dict["HIGHFREQUENCY"]}_numpolicies={NUM_POLICIES}.pickle'
+fairness_values = [fairness_index[i][len(data) - 1] for i in range(len(policies) - 1)]
+alphaFairCBValue = fairness_index[len(policies) - 1][len(data) - 1]
+with open(FAIRNESS_VALUES_FILE, 'wb') as f:
+    pickle.dump({"fairness_values": fairness_values, "alphaFairCBValue": alphaFairCBValue}, f)
+
+# ## plotting the fairness levels
+# import matplotlib.pyplot as plt
+# USETEXLIVE = config_dict["USETEXLIVE"]
+
+# if USETEXLIVE:
+#     plt.rc('text', usetex=True)
+#     plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
+# plt.style.use('seaborn-v0_8-darkgrid')
+# plt.rcParams["figure.figsize"] = (5, 4)
+
+# JAINS_FAIRNESS_PLOT_PATH = "plots/jains_index_varying_nus.pdf"
+
+# plt.figure(0)
+# ax = plt.axes()
+# plt.plot(nus, fairness_values)
+# plt.axhline(y=fairness_index[NUM_POLICIES - 1][len(data) - 1])
+# plt.xlabel("nu", fontsize="large")
+# plt.ylabel("Jain's Fairness Index", fontsize="large")
+# # if USETEXLIVE:
+# #     plt.text(0.5, 0.95, f"$\\alpha={ALPHA}$, $\\nu = {config_dict['FAIRCBFAIRNESS']}$, $\\delta={SMALL_REWARD}$", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+# # plt.title("Jain's Fairness Index Plot (Full Information Setting)", fontsize="large")
+# plt.savefig(JAINS_FAIRNESS_PLOT_PATH, bbox_inches='tight', pad_inches=0.01)
+
+
