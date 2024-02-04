1f7e52bf75a397251068eebe7ded99eab7962ea8
Siddhant Chaudhary <urssidd@gmail.com>
2024-02-04 12:25:57 +0530

    Cleaning up experiments for varying values of nu.

--- ml25m/dependence_on_mu.py
+++ ml25m/dependence_on_mu.py
@@ -1,19 +1,7 @@
-from driver import *
-
-NUM_POLICIES = 51
-
-from ParallelOPF import ParallelOPF
-from FairCB import FairCB
-from utils import jains_fairness_index
-
-## getting the context distribution for fairCB
-valueCounts = data["userId"].value_counts()
-context_distribution = np.zeros((NUM_CONTEXTS, ))
-for context_id in range(NUM_CONTEXTS):
-    context_distribution[context_id] = valueCounts.loc[map_index_to_user[context_id]] / len(data)
+from driver_full_information import *
 
 ## will have one FairCB policy for each nu
-nus = np.linspace(0, 1, NUM_POLICIES).tolist()[:-1]
+nus = np.linspace(0, 1, NUM_NUS + 1).tolist()[:-1]
 policies = [
     FairCB(NUM_CONTEXTS, NUM_ARMS, nus[i] / NUM_ARMS, len(data), context_distribution)
     for i in range(len(nus))
@@ -50,33 +38,13 @@ for t in tqdm(range(len(data))):
     for i in range(len(policies)):
         policies[i].feedback(rewards)
 
-FAIRNESS_VALUES_FILE = f'pickled_files/fairness_values_rows={config_dict["ROWS"]}_seed={SEED}_alpha={ALPHA}_smallreward={SMALL_REWARD}_usetimestamps={USETIMESTAMPS}_frequency={config_dict["FREQUENCY"]}_highfrequency={config_dict["HIGHFREQUENCY"]}_numpolicies={NUM_POLICIES}.pickle'
 fairness_values = [fairness_index[i][len(data) - 1] for i in range(len(policies) - 1)]
 alphaFairCBValue = fairness_index[len(policies) - 1][len(data) - 1]
 with open(FAIRNESS_VALUES_FILE, 'wb') as f:
-    pickle.dump({"fairness_values": fairness_values, "alphaFairCBValue": alphaFairCBValue}, f)
-
-# ## plotting the fairness levels
-# import matplotlib.pyplot as plt
-# USETEXLIVE = config_dict["USETEXLIVE"]
-
-# if USETEXLIVE:
-#     plt.rc('text', usetex=True)
-#     plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
-# plt.style.use('seaborn-v0_8-darkgrid')
-# plt.rcParams["figure.figsize"] = (5, 4)
-
-# JAINS_FAIRNESS_PLOT_PATH = "plots/jains_index_varying_nus.pdf"
-
-# plt.figure(0)
-# ax = plt.axes()
-# plt.plot(nus, fairness_values)
-# plt.axhline(y=fairness_index[NUM_POLICIES - 1][len(data) - 1])
-# plt.xlabel("nu", fontsize="large")
-# plt.ylabel("Jain's Fairness Index", fontsize="large")
-# # if USETEXLIVE:
-# #     plt.text(0.5, 0.95, f"$\\alpha={ALPHA}$, $\\nu = {config_dict['FAIRCBFAIRNESS']}$, $\\delta={SMALL_REWARD}$", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-# # plt.title("Jain's Fairness Index Plot (Full Information Setting)", fontsize="large")
-# plt.savefig(JAINS_FAIRNESS_PLOT_PATH, bbox_inches='tight', pad_inches=0.01)
-
-
+    pickle.dump({
+        "fairness_values": fairness_values,
+        "alphaFairCBValue": alphaFairCBValue,
+        "nus": nus
+        },
+        f
+    )
