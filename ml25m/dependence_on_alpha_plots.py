826aa559d57396ff4d8225a3c9cdf7698e410023
Siddhant Chaudhary <urssidd@gmail.com>
2024-02-06 22:26:55 +0530

    More beautification of plots.

--- ml25m/dependence_on_alpha_plots.py
+++ ml25m/dependence_on_alpha_plots.py
@@ -13,6 +13,10 @@ plt.style.use('seaborn-v0_8-darkgrid')
 plt.rcParams["figure.figsize"] = (5, 4)
 
 plt.figure(0)
+ax = plt.axes()
+ax.patch.set_edgecolor('black')  
+ax.patch.set_linewidth(1)  
+ax.tick_params(direction='out', length=5, width=0.5, grid_alpha=0.5)
 plt.plot(alphas, [cumulative_rewards[i][-1].sum() / NUM_ARMS for i in range(len(alphas))])
 plt.legend(loc="upper left", fontsize="large")
 if USETEXLIVE: 
@@ -23,7 +27,11 @@ plt.ylabel("Average Cumulative Rewards", fontsize="large")
 plt.savefig(AVERAGED_CUMULATIVE_REWARDS_PATH)
 
 plt.figure(2)
-plt.plot(alphas, [jains_fairness_index(cumulative_rewards[i][-1]) for i in range(len(alphas))], label=r"$\alpha$-\textsc{FairCB}")
+ax = plt.axes()
+ax.patch.set_edgecolor('black')  
+ax.patch.set_linewidth(1)  
+ax.tick_params(direction='out', length=5, width=0.5, grid_alpha=0.5)
+plt.plot(alphas, [jains_fairness_index(cumulative_rewards[i][-1]) for i in range(len(alphas))], color='b')
 plt.legend(loc="upper left", fontsize="large")
 if USETEXLIVE: 
     plt.xlabel(r"$\alpha$", fontsize="large")
