2a553b44a3e6f0bcbdf459e9276792cbfe1d8f9c
Siddhant Chaudhary <urssidd@gmail.com>
2024-04-04 02:27:55 +0530

    Updating steps.

--- ml25m/steps.sh
+++ ml25m/steps.sh
@@ -2,4 +2,10 @@ python3 -m config --ROWS=50000 --SEED=1 --ALPHA=0.9 --SMALLREWARD=0.001 --USETIM
 python3 -m data_exploration
 python3 -m offline_optimal_optimized
 python3 -m ml25m
+python3 -m ml25m_plots
 python3 -m ml25m_bandit
+python3 -m ml25m_bandit_plots
+python3 -m dependence_on_alpha
+python3 -m dependence_on_alpha_plots
+python3 -m dependence_on_nu
+python3 -m dependence_on_nu_plots
